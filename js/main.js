e2f459e86df139cab48d10b1d71403356b645d93
grivastova <grivastova05@mail.ru>
2015-12-07 17:11:19 +0200

    firstcommit

--- js/main.js
+++ js/main.js
@@ -38,111 +38,102 @@ function second(){
 	
 
 	else if(b>a && a>c && c>d){
-		b=b*20
-		a=a*10
-		c=c*5
+		b=b*20;
+		a=a*10;
+		c=c*5;
 	}
 	else if(b>c && c>a && a>d){
-		b=b*20
-		c=c*10
-		a=a*5
+		b=b*20;
+		c=c*10;
+		a=a*5;
 	}
 	else if(b>d && d>c && c>a){
-		b=b*20
-		d=d*10
-		c=c*5
+		b=b*20;
+		d=d*10;
+		c=c*5;
 	}
 	else if(b>c && c>d && d>a){
-		b=b*20
-		c=c*10
-		d=d*5
+		b=b*20;
+		c=c*10;
+		d=d*5;
 	}
 	else if(b>d && d>a && a>c){
-		b=b*20
-		d=d*10
-		a=a*5
+		b=b*20;
+		d=d*10;
+		a=a*5;
 	}
 	else if(b>a && a>d && d>c){
-		b=b*20
-		a=a*10
-		d=d*5
+		b=b*20;
+		a=a*10;
+		d=d*5;
 	}
 
 	
 
 	else if (c>a && a>b && b>d) {
-		c=c*20
-		a=a*10
-		b=b*5
+		c=c*20;
+		a=a*10;
+		b=b*5;
 	}
 	else if (c>a && a>d && d>b) {
-		c=c*20
-		a=a*10
-		d=d*5
+		c=c*20;
+		a=a*10;
+		d=d*5;
 	}
 	else if (c>b && b>a && a>d) {
-		c=c*20
-		b=b*10
-		a=a*5
+		c=c*20;
+		b=b*10;
+		a=a*5;
 	}
 	else if (c>b && b>d && d>a) {
-		c=c*20
-		b=b*10
-		d=d*5
+		c=c*20;
+		b=b*10;
+		d=d*5;
 	}
 	else if (c>d && d>b && b>a) {
-		c=c*20
-		d=d*10
-		b=b*5
+		c=c*20;
+		d=d*10;
+		b=b*5;
 	}
 	else if (c>d && d>a && a>b) {
-		c=c*20
-		d=d*10
-		a=a*5
+		c=c*20;
+		d=d*10;
+		a=a*5;
 
 	}
 	
 
 	else if (d>a && a>b && b>c) {
-		d=d*20
-		a=a*10
-		b=b*5
+		d=d*20;
+		a=a*10;
+		b=b*5;
 	}
 	else if (d>a && a>c && c>b) {
-		d=d*20
-		a=a*10
-		c=c*5
+		d=d*20;
+		a=a*10;
+		c=c*5;
 	}
 	else if (d>b && b>a && a>c) {
-		d=d*20
-		b=b*10
-		a=a*5
+		d=d*20;
+		b=b*10;
+		a=a*5;
 	}
 	else if (d>b && b>c && c>a) {
-		d=d*20
-		b=b*10
-		c=c*5
+		d=d*20;
+		b=b*10;
+		c=c*5;
 	}
 	else if (d>c && c>a && a>b) {
-		d=d*20
-		c=c*10
-		a=a*5
+		d=d*20;
+		c=c*10;
+		a=a*5;
 	}
 	else if (d>c && c>b && b>a) {
-		d=d*20
-		c=c*10
-		b=b*5
+		d=d*20;
+		c=c*10;
+		b=b*5;
 	}
 }
-
-
-
-
-
-
-
-
-
 /*
 	if(a > b && a > c && a > d){
 		a = a * 20;
@@ -174,12 +165,8 @@ function second(){
 			}
 		}
 	}
-*/
 }
-
-
-
-
+*/
 
 function first(){
 	if(a > b && b > c){
@@ -210,3 +197,11 @@ function first(){
 
 first();
 console.log("a = " + a + " b = " + b + " c = " + c);
+a = 10;
+b = 30;
+c = 20;
+d = 5;
+console.log("a = " + a + " b = " + b + " c = " + c);
+second();
+
+console.log("a = " + a + " b = " + b + " c = " + c + " d = " + d);
